efaa9c6b8455d587362fd845b95786d575cf9efd
NICOLAS MACKBOOK PRO <nicerlion@gmail.com>
2019-03-15 06:35:28 -0500

    hero borders and hamburguer menu

--- src/parts/shared/hero-navigation.php
+++ src/parts/shared/hero-navigation.php
@@ -1,25 +1,34 @@
 <header
   id="header-style-2"
   class="torque-header hero-overlay">
+  <div class="torque-header-overlay-container">
+    <div class="row torque-header-content-wrapper torque-navigation-toggle">
 
-  <div class="row torque-header-content-wrapper torque-navigation-toggle">
+      <div class="torque-header-burger-menu-wrapper">
+        <?php get_template_part( 'parts/elements/element', 'burger-menu-squeeze'); ?>
+      </div>
 
-    <div class="torque-header-burger-menu-wrapper">
-      <?php get_template_part( 'parts/elements/element', 'burger-menu-squeeze'); ?>
-    </div>
-
-    <div class="header-links-wrapper">
-      <div class="torque-header-menu-items-inline-wrapper">
-        <?php get_template_part( 'parts/shared/header-parts/menu-items/menu-items', 'inline'); ?>
+      <div class="header-links-wrapper">
+        <div class="torque-header-menu-items-inline-wrapper">
+          <?php get_template_part( 'parts/shared/header-parts/menu-items/menu-items', 'inline'); ?>
+        </div>
       </div>
     </div>
 
-  </div>
+    <div class="torque-header-logo-wrapper">
+      <?php get_template_part( 'parts/shared/logo', 'white' ); ?>
+    </div>
 
-  <div class="torque-header-logo-wrapper">
-    <?php get_template_part( 'parts/shared/logo', 'white' ); ?>
+    <div class="hero-left-border">
+      <div class="hero-left-sub-border"></div>
+    </div>
+    <div class="hero-right-border">
+      <div class="hero-right-sub-border"></div>
+    </div>
+    <div class="hero-center-bottom-border">
+      <div class="hero-center-bottom-sub-border"></div>
+    </div>
   </div>
-
   <div class="torque-navigation-toggle torque-header-menu-items-mobile">
     <?php get_template_part( 'parts/shared/header-parts/menu-items/menu-items', 'stacked'); ?>
   </div>
