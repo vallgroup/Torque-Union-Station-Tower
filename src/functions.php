cc5cce31e6de6b56538b0d306ad4f7159c1ff9c2
mattwills8 <matt_wills8@hotmail.co.uk>
2019-03-20 15:43:20 -0600

    add slideshow plugin configuration

--- src/functions.php
+++ src/functions.php
@@ -46,6 +46,31 @@ if ( class_exists( 'UnionStationTower_Customizer' ) ) {
    new UnionStationTower_Locations_CPT();
  }
 
+
+ /**
+ * Slideshow plugin settings
+ */
+
+ if ( class_exists( 'Torque_Slideshow' ) ) {
+   add_filter( Torque_Slideshow::$USE_POST_SLIDESHOW_FILTER_HOOK, function() { return true; });
+
+   if ( class_exists( 'Torque_Post_Slideshow_CPT' ) ) {
+     add_filter( Torque_Post_Slideshow_CPT::$RELATIONSHIP_FIELD_FILTER_HOOK, function($field) {
+
+       $field['post_type'] = array(
+         0 => Torque_Floor_Plan_CPT::$floor_plan_labels['post_type_name'],
+         1 => UnionStationTower_Locations_CPT::$locations_labels['post_type_name'],
+       );
+
+       $field['filters'] = array(
+				 0 => 'post_type',
+			 );
+
+       return $field;
+     });
+   }
+ }
+
  /**
  * Floor Plan Plugin Settings
  */
