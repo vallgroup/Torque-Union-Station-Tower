c0ee6614ea19c0d32ae8f358cc8810d888f1bb1b
mattwills8 <matt_wills8@hotmail.co.uk>
2019-03-20 17:34:44 -0600

    add location CPT content meta box

--- src/includes/union-station-tower-locations-cpt-class.php
+++ src/includes/union-station-tower-locations-cpt-class.php
@@ -31,7 +31,6 @@ class UnionStationTower_Locations_CPT {
 	protected $locations_options = array(
 		'supports' => array(
 			'title',
-      'excerpt',
       'thumbnail'
 		),
 		'menu_icon'           => 'dashicons-editor-contract',
@@ -45,6 +44,53 @@ class UnionStationTower_Locations_CPT {
 		if ( class_exists( 'PremiseCPT' ) ) {
 			new PremiseCPT( self::$locations_labels, $this->locations_options );
 		}
+
+    if( function_exists('acf_add_local_field_group') ):
+
+      acf_add_local_field_group(array(
+      	'key' => 'group_5c92bc526e40d',
+      	'title' => 'Location Fields',
+      	'fields' => array(
+      		array(
+      			'key' => 'field_5c92bc5cf92f8',
+      			'label' => 'Content',
+      			'name' => 'content',
+      			'type' => 'wysiwyg',
+      			'instructions' => '',
+      			'required' => 0,
+      			'conditional_logic' => 0,
+      			'wrapper' => array(
+      				'width' => '',
+      				'class' => '',
+      				'id' => '',
+      			),
+      			'default_value' => '',
+      			'tabs' => 'all',
+      			'toolbar' => 'basic',
+      			'media_upload' => 0,
+      			'delay' => 0,
+      		),
+      	),
+      	'location' => array(
+      		array(
+      			array(
+      				'param' => 'post_type',
+      				'operator' => '==',
+      				'value' => 'torque_location',
+      			),
+      		),
+      	),
+      	'menu_order' => 0,
+      	'position' => 'normal',
+      	'style' => 'default',
+      	'label_placement' => 'top',
+      	'instruction_placement' => 'label',
+      	'hide_on_screen' => '',
+      	'active' => 1,
+      	'description' => '',
+      ));
+
+      endif;
 	}
 }
 
