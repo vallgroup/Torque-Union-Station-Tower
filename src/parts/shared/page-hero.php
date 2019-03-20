1e47ba7751f4441053603d340dc4ef46413571f0
NICOLAS MACKBOOK PRO <nicerlion@gmail.com>
2019-03-20 08:39:59 -0500

    Hero with feature image and hero with no image styles

--- src/parts/shared/page-hero.php
+++ src/parts/shared/page-hero.php
@@ -2,30 +2,29 @@
 
 $type = get_field('hero_type');
 
+if (!$type) {
+  $type = 'none';
+}
+
 $image = get_field('hero_image');
 $slideshow = get_field('hero_slideshow');
 
 $overlay_type = get_field('hero_overlay_type');
 $text_overlay = get_field('hero_text_overlay');
 
-
-if (($type === 'image' && $image) || ($type === 'slideshow' && $slideshow)) { ?>
+?>
 
 <div class="page-hero type-<?php echo $type; ?>">
 
-  <?php if ($image) { ?>
+  <?php if ($type === 'image' and $image) { ?>
     <div class="hero-image-size">
       <div class="hero-image" style="background-image: url(<?php echo $image; ?>);" ></div>
     </div>
-  <?php } else {
+  <?php } else if ($type === 'slideshow' and $slideshow) {
     echo do_shortcode($slideshow);
   } ?>
 
   <?php include locate_template('/parts/shared/hero-navigation.php'); ?>
 </div>
 
-<?php } else { ?>
-
 <div class="page-hero-filler" ></div>
-
-<?php } ?>
