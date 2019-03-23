2bb504eb9a3d9a054a9efc0f44985b63b2e90854
NICOLAS MACKBOOK PRO <nicerlion@gmail.com>
2019-03-23 13:19:25 -0500

    Blog post started

---
+++ src/parts/templates/titles/title.php
@@ -0,0 +1,10 @@
+<div class="tq-title tq-post-title">
+
+  <h1>
+    <?php the_title(); ?>
+  </h1>
+
+  <h3>Written by <?php the_author(); ?></h3>
+  <p>December 20th, 2018 <?php the_time('F j, Y'); ?></p>
+  
+</div>
---
+++ src/single.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * The Template for displaying all single posts
+ *
+ * @package 		torque
+ * @author 			Torque
+ */
+?>
+<?php TQ::get_template_parts( array( 'parts/shared/html-header' ) ); ?>
+
+<main>
+	<?php
+	if ( have_posts() ) {
+		while ( have_posts() ) {
+			the_post();
+	?>
+		<?php get_template_part( 'parts/templates/titles/title-page', get_post_format() ); ?>
+
+		<div class="post-container">
+			<div class="row">
+				<div class="post-data">
+					<?php get_template_part( 'parts/templates/titles/title', get_post_format() ); ?>
+					<?php get_template_part( 'parts/templates/post-thumbnail', get_post_format() ); ?>
+					<?php get_template_part( 'parts/templates/content', get_post_format() ); ?>
+				</div>
+				<div class="post-line"><div>
+				<?php get_template_part( 'sidebar-primary', get_post_format() ); ?>
+			</div>
+		</div>
+	<?php
+		}
+	}
+	else {  ?>
+
+		<h1>
+			<?php echo 'Nothing to show yet.'; ?>
+		</h1>
+
+	<?php } ?>
+</main>
+
+<?php TQ::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
