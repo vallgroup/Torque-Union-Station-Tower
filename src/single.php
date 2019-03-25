e3d9dec6b2a915d0bdda19a593e60b905f752937
NICOLAS MACKBOOK PRO <nicerlion@gmail.com>
2019-03-25 17:36:12 -0500

    Blog Post and Recent Post

--- src/single.php
+++ src/single.php
@@ -17,7 +17,6 @@
 		<?php get_template_part( 'parts/templates/titles/title-page', get_post_format() ); ?>
 
 		<div class="post-container">
-			
 				<div class="blog-post-data">
 					<?php get_template_part( 'parts/templates/titles/title', get_post_format() ); ?>
 					<?php get_template_part( 'parts/templates/post-thumbnail', get_post_format() ); ?>
@@ -25,8 +24,29 @@
 				</div>
 				<div class="blog-post-sidebar">
 					<?php get_template_part( 'sidebar-primary', get_post_format() ); ?>
+					<h3>KEEP READING</h3>
+					<div class="sidebar-primary-container">
+						<?php
+							$args = array( 
+								'numberposts' => '4',
+								'post__not_in' => array( $post->ID )
+							);
+							$recent_posts = wp_get_recent_posts( $args );
+							
+							foreach( $recent_posts as $recent ){ 
+								echo '<div class="col2-tablet col1-desktop recent-post-box">';
+								if ( has_post_thumbnail( $recent["ID"]) ) {
+									echo  '<a href="' . get_permalink($recent['ID']) . '">' . get_the_post_thumbnail($recent["ID"],'medium'). '</a>';
+								}
+								echo '<a href="' . get_permalink($recent['ID']) . '" class="recent-post-title">' .   $recent['post_title'].'</a>';
+								echo '<div class="recent-post-excerpt">'. $recent['post_excerpt'] .'</div>';
+								echo '<a href="'. get_permalink($recent['ID']).'" class="post-read-more">READ MORE</a>';
+								echo '</div>';
+							}
+							wp_reset_query();
+						?>
+					</div>
 				<div>
-			
 		</div>
 	<?php
 		}
@@ -39,5 +59,4 @@
 
 	<?php } ?>
 </main>
-
 <?php TQ::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
